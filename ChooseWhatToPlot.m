79b05dc69c3f256d71efce890f3ba564f6aec66c
Steven Smeal <steven.smeal@gmail.com>
2020-04-21 10:50:18 -0400

    Updated the code so we can easily switch between log and nonlog plots

--- ChooseWhatToPlot.m
+++ ChooseWhatToPlot.m
@@ -2,11 +2,12 @@ MainOutputFolder = "../../../Box/PhD/Spring_2020_Classes/Cell_and_Systems/CellSy
 CurrentTime = clock;
 DateDir = CurrentTime(1)+"_"+CurrentTime(2)+"_"+CurrentTime(3)+"___";
 TimeDir = CurrentTime(4)+"_"+CurrentTime(5)+"_"+floor(CurrentTime(6))+"___"; 
-PersonalLabel = "Avoid7_UpdateLegend"; 
-
+PersonalLabel = "Avoid7_UpdateLegend_Log10"; 
+%PersonalLabel = "Exclude_2_10_1_11_UpdateLegend_Log10";
 
 OutputDirectory = MainOutputFolder+DateDir+TimeDir+PersonalLabel;
 
+Log10Plot = true; 
 Status = mkdir(OutputDirectory);
 
 %Clustering 1 
@@ -52,19 +53,33 @@ for ClusterNum = 1:length(PlotListList)
         OrginalAnalyte = GetSimulatedData(Analyte,OrginalModelObservables,OrginalModelInfo);
         Samples(1,ColAnalyte) = OrginalAnalyte(end);
         figure 
-        plot(timepointsMinutes,OrginalAnalyte,"--","DisplayName","Orginal","LineWidth",4)
+        if Log10Plot
+            plot(timepointsMinutes,log10(abs(OrginalAnalyte)),"--","DisplayName","Orginal","LineWidth",4)
+        else 
+            plot(timepointsMinutes,OrginalAnalyte,"--","DisplayName","Orginal","LineWidth",4)
+        end 
         hold on 
         for i = 1:length(GeneratedModelsShortList)
             NextAnalytesToPlot = GetSimulatedData(Analyte,DataStoreShortList{i},ModelInfoStoreShortList{i});
             Samples(i+1,ColAnalyte) = NextAnalytesToPlot(end);
             ModelToRun = split(GeneratedModelsShortList{i},".");
             ModelToRun = ModelToRun{1};
-            plot(timepointsMinutes,NextAnalytesToPlot,"DisplayName",ModelToRun,"LineWidth",4)
+            if Log10Plot
+                plot(timepointsMinutes,log10(abs(NextAnalytesToPlot)),"DisplayName",ModelToRun,"LineWidth",4)
+            else 
+                plot(timepointsMinutes,NextAnalytesToPlot,"DisplayName",ModelToRun,"LineWidth",4)
+            end 
         end 
         legend('Location','NorthEastOutside','Interpreter', 'none',"FontSize",20)
         grid
         xlabel("Time [Minutes]","FontSize",20)
-        ylabel(Analyte,"FontSize",20)
+        if Log10Plot
+            ylabel("Log_{10}("+Analyte+")","FontSize",20)
+            ylim([0 inf])
+        else 
+            ylabel(Analyte,"FontSize",20)
+            ylim([0 inf])
+        end 
         SAVDIR = OutputDirectory + "/";
         Specific = "Cluster_"+num2str(ClusterNum-1) + "___" + Analyte + ".png";
         SaveName = SAVDIR+Specific;
