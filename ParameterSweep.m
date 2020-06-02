e68f97682706ef4f7653185dbc898bd211774544
Steven Smeal <steven.smeal@gmail.com>
2020-06-02 14:16:57 -0400

    Added functionality where the user can directly select points to analyze.

--- ParameterSweep.m
+++ ParameterSweep.m
@@ -7,11 +7,12 @@ close all;
 
 
 
-num_of_samples = 500; 
-sigma = 2; 
+num_of_samples = 50; 
+sigma = 1; 
 ModelName = "Model";
+Save_Directory = "TempSaveDirectory";
 
-num_of_mutations_list = [1 2 4 8 10 16 32 65];
+num_of_mutations_list = [1];
 
 for num_of_mutations = num_of_mutations_list
 disp("Working on simulation with " + num2str(num_of_mutations) + " number of mutations")
@@ -72,7 +73,7 @@ PC2 = score(:,2);
 
 Phage = DataObservation(:,end);
 figure 
-scatter(PC1,PC2,[],Phage,'filled')
+pcaFigure = scatter(PC1,PC2,[],Phage,'filled');
 colorbar
 xlabel("Principal Component 1","FontSize",20) 
 ylabel("Principal Component 2","FontSize",20)
@@ -83,7 +84,42 @@ drawnow
 VectorLengths = sqrt(PC1.^2 + PC2.^2);
 [values,idx] = max(VectorLengths);
 
+
+%==========================================================================
+%                     Select Point for Analysis 
+%==========================================================================
+ReRun = 1; 
+selectionNumber = 0; 
+disp("Click on the simulations you wish to study further and click enter")
+disp("If you click multiple times before pressing enter, only the last click"+ ...
+     "will be used to record")
+disp("Press enter twice to exit and save")
+while ReRun
+    selectionNumber = selectionNumber+1;
+    [Coordinates] = ginput;
+
+    if isempty(Coordinates)
+        ReRun = 0; 
+        saveas(pcaFigure,Save_Directory+"/PCA____Samples_"+num2str(num_of_samples)+"_Mutations_"+num2str(num_of_mutations)+".png");
+    else 
+        %Default is the last click: 
+        x = Coordinates(end,1);
+        y = Coordinates(end,2); 
+        
+        %Euclidean distance: 
+        [~,IDX] = min(sqrt((PC1-x).^2 + (PC2-y).^2));
+        
+        hold on 
+        scatter(PC1(IDX),PC2(IDX),80)
+        text(PC1(IDX),PC2(IDX),"\leftarrow "+num2str(selectionNumber))
+    end 
 end 
+
+end 
+
+
+
+
 
 function [param_labels,parameters,observable_labels] = GetModelInformation(model)
     % Get parameter names from param_labels string definition
